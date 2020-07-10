ed27e2447627cc04610bbda5f1e44957845c1bc4
Wilmar Reyes <reyes.wilmar@gmail.com>
2020-07-10 23:21:12 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,6 +30,11 @@ end
 
 def initial_round
   # code #initial_round here
+  card_one = deal_card
+  card_two = deal_card
+  card_total = card_one + card_two
+  display_card_total
+  return card_total
 end
 
 def hit?
