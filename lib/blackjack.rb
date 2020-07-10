82e6ae93a08405ff5419b5f40e5a58f230c53be8
Wilmar Reyes <reyes.wilmar@gmail.com>
2020-07-10 23:27:00 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -28,10 +28,12 @@ def end_game(card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
-def initial_round(deal_card)
+def initial_round
   # code #initial_round here
+  deal_card
+  deal_card
   display_card_total
-  return card_total
+
 end
 
 def hit?
