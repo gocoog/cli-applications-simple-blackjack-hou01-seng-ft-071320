8689e243334f7fe87891ff57213d2de76c08e984
Wilmar Reyes <reyes.wilmar@gmail.com>
2020-07-10 23:39:28 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -37,10 +37,21 @@ end
 
 def hit?(card_total)
   # code hit? here
+  prompt_user
+  get_user_input
+  if get_user_input == 's'
+    break
+  elsif get_user_input == 'h'
+    card_total + deal_card
+  else
+    invalid_command
+    prompt_user
+  end
 end
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
